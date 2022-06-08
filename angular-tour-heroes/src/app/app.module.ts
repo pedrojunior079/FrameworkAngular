3ff8e53efd664e4d35a0f4508f322f27f57c0489
Pedro Arns Junior <pedroarnsjr@outlook.com>
2022-06-08 16:01:13 -0300

    Atualização

--- angular-tour-heroes/src/app/app.module.ts
+++ angular-tour-heroes/src/app/app.module.ts
@@ -4,16 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent,
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-tour-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-heroes/src/app/heroes/heroes.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import {Hero} from '../hero';
+import { Hero } from '../hero';
+import {HeroService} from '../hero.service';
+
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    nome: 'Windstorm'
-  }; 
-
-  constructor() { }
-
-  ngOnInit(): void {
     
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
